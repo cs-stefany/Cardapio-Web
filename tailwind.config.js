a379506f26e9296cae1ca6e36d6048d906c73c47
Stefany-Teffy <stefanycsouza26@gmail.com>
2026-01-26 18:21:54 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
